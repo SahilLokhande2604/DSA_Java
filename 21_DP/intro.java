7887495efbc02b3c528a36145c879b22af3c359f
SahilLokhande2604 <148880909+SahilLokhande2604@users.noreply.github.com>
2024-08-23 10:24:49 +0530

    Update intro.java

--- 21_DP/intro.java
+++ 21_DP/intro.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class intro{
     public static void main(String a[]){
-        System.out.println("Hello Welcom to DP");
+        System.out.println("Hello Welcome to DP");
     }
 }
